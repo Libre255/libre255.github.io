1ea2d4b4b744c91f64d0512f7790c7bbea0d113b
Libre255 <briangbv@outlook.com>
2021-08-09 19:39:49 +0200

    asd

--- server/cards/cardsAPI.js
+++ server/cards/cardsAPI.js
@@ -4,7 +4,7 @@ const {getCards} = require('./getCards')
 const cardsAPI = express.Router();
 
 cardsAPI.get('/', (req, res)=>{
-  getCards().then(cardsArray => res.json(cardsArray))
+  getCards().then(cardsArray => res.json(cardsArray)).catch(error => res.status(404).json('Didnt work fetching my array :C'))
 })
 
 
